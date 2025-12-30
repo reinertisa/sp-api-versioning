6f89777b3a824a013a054fa0215791ce96796a60
Isa Reinert <reinert.isa@gmail.com>
2025-12-30 01:12:26 -0800

    feat(api): create user controller

---
+++ src/main/java/com/reinertisa/api/UserController.java
@@ -0,0 +1,38 @@
+package com.reinertisa.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    public UserController(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    @GetMapping(value = "/{version}/users", version = "1.0")
+    public List<UserDTOv1> findAllUserV1() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toV1)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{version}/users", version = "2.0")
+    public List<UserDTOv2> findAllUserV2() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toV2)
+                .collect(Collectors.toList());
+    }
+}
